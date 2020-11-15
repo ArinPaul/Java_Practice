e5e07e58ac38d24bd7375563d73cbbed7685e2a7
ArinPaul <arinpaul56@gmail.com>
2020-11-16 00:22:16 +0530

    Pattern 10

--- Pattern/Pattern_10.java
+++ Pattern/Pattern_10.java
@@ -1,9 +1,26 @@
 package Pattern;
-
+import java.util.*;
 public class Pattern_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nsp = 0, nst = 2*n-1;
+		for(int i=0;i<n;i++)
+		{
+			for(int j = 0;j<nsp;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j = 0;j<nst;j++)
+			{
+				System.out.print("* ");
+			}
+			nsp++;
+			nst-=2;
+			System.out.println();
+		}
+		sc.close();
 
 	}
 
