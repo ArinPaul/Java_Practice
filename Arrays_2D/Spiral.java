703b70a7a30769b7e4027070e63e7a524b6788c8
ArinPaul <arinpaul56@gmail.com>
2020-11-29 02:11:48 +0530

    Spiral

--- Arrays_2D/Spiral.java
+++ Arrays_2D/Spiral.java
@@ -26,6 +26,64 @@ public class Spiral {
 		return arr;
 	}
 	public static void spiral(int[][] arr) {
+		int top = 0, bottom = arr.length-1, left = 0, right = arr[top].length-1;
+		int count = (bottom+1)*(right+1), dir = 1;
+		
+		while(left <= right && top <= bottom)
+		{
+			if(count > 0)
+			{
+				if(dir == 1)
+				{
+					for(int i=left;i<=right;i++)
+					{
+						System.out.print(arr[top][i]+" ");
+						count--;
+					}
+					top++;
+					dir = 2;
+				}				
+			}
+			if(count > 0)
+			{
+				if(dir == 2)
+				{
+					for(int i=top;i<=bottom;i++)
+					{
+						System.out.print(arr[i][right]+" ");
+						count--;
+					}
+					right--;
+					dir = 3;
+				}				
+			}
+			if(count > 0)
+			{
+				if(dir == 3)
+				{
+					for(int i=right;i>=left;i--)
+					{
+						System.out.print(arr[bottom][i]+" ");
+						count--;
+					}
+					bottom--;
+					dir = 4;
+				}				
+			}
+			if(count > 0)
+			{
+				if(dir == 4)
+				{
+					for(int i=bottom;i>=top;i--)
+					{
+						System.out.print(arr[i][left]+" ");
+						count--;
+					}
+					left++;
+					dir = 1;
+				}				
+			}
+		}
 		
 	}
 }
